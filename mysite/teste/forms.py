33de60eb2d57070b85859e68014bdee971692e86
Bruno Adonis <adonisdesa@hotmail.com>
2016-06-09 14:45:36 -0300

    Bruno - Forms
    formularios dos models

--- mysite/teste/forms.py
+++ mysite/teste/forms.py
@@ -1,9 +1,67 @@
 from django import forms
-
-from .models import Post
+#from .models import Post
+#from models import .
+import models
+from django.forms import ModelForm
 
 class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
         fields = ('title', 'text',)
+
+class EquipamentoForm(ModelForm):
+	class Meta:
+		model = Equipamento
+		fields = ['nome','descricao']
+
+class CidadeForm(ModelForm):
+	class Meta:
+		model = Cidade
+		fields = ['nome','uf','pais']
+
+class UfForm(ModelForm):
+	class Meta:
+		model = Uf
+		fields = ['sigla','pais','nome']
+
+class PaisForm(ModelForm):
+	class Meta:
+		model = Pais
+		fields = ['sigla','nome']
+
+class FornecedorForm(ModelForm):
+	class Meta:
+		model = Fornecedor
+		fields = ['nome','descricao','nome_logradouro',
+		'complemento','bairro',
+		'cep','telefone','email']
+
+class MaterialForm(ModelForm):
+	class Meta:
+		model = Material
+		fields = ['nome','preco','qtd_estoque']
+
+class PagamentoForm(ModelForm):
+	class Meta:
+		model = Pagamento
+		fields = ['valor','data_pagamento','tipo_pagamento']
+
+class SetorForm(ModelForm):
+	class Meta:
+		model = Setor
+		fields = ['nome','sigla','responsavel']
+
+class TarefaForm(ModelForm):
+	class Meta:
+		model = Tarefa
+		fields = ['nome','descricao','prazo']
+
+class FuncionarioForm(ModelForm):
+	class Meta:
+		model = Funcionario
+		fields = ['cpf','nome','sexo','estado_civil',
+		'sangue_fator','sangue_rh','tipo_logradouro',
+		'nome_logradouro','complemento','bairro',
+		'cep','telefone','data_nascimento','admissao',
+		'email','salario']
